70e2eb69dc8c269acda08059a1a45e3ebdfc5e54
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-03 15:13:50 +1000

    Add click event to naught or cross
    Add a click event to add either a naught or cross class to a given Cell,
    if it does not already have one assigned, and then toggle the active
    player.

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('app.js loaded')
+console.log('app.js loaded');
+
+// var playerTurn = 'naught';
+// var opponentTurn = 'cross';
+var players = {
+  active: 'naught',
+  inactive: 'cross'
+}
+
+var board = document.querySelector('.board');
+var cells = document.querySelector('.cell');
+
+board.addEventListener('click', function(event) {
+  if (!event.target.classList.contains('cell')) { return; }
+  if (event.target.classList.contains('naught') ||
+      event.target.classList.contains('cross')) { return; }
+  event.target.classList.add(players.active);
+  togglePlayerTurn();
+})
+
+function togglePlayerTurn() {
+  var tempActive = players.active;
+  var tempInactive = players.inactive;
+
+  players = {
+    active: tempInactive,
+    inactive: tempActive
+  };
+}
