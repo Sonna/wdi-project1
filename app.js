f156ea67d401608cb34c0069a0e4c32d7cb855c8
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-03 15:26:16 +1000

    Add initial solution to Naughts & Crosses project
    Add initial solution to implementing Naughts and Crosses game for
    General Assembly Project 1, which naively implements the board cell
    checking to determine a winner.

--- app.js
+++ app.js
@@ -7,18 +7,27 @@ var players = {
   inactive: 'cross'
 }
 
+var running = true;
+var winner = '';
+
 var board = document.querySelector('.board');
-var cells = document.querySelector('.cell');
+// var cells = document.querySelectorAll('.cell');
+var rows = document.querySelectorAll('.row');
+
+var winnerEl = document.querySelector('.winner');
 
 board.addEventListener('click', function(event) {
+  if (!running) { return; }
   if (!event.target.classList.contains('cell')) { return; }
   if (event.target.classList.contains('naught') ||
       event.target.classList.contains('cross')) { return; }
   event.target.classList.add(players.active);
+  checkForWinner(players.active);
   togglePlayerTurn();
 })
 
 function togglePlayerTurn() {
+  if (!running) { return; }
   var tempActive = players.active;
   var tempInactive = players.inactive;
 
@@ -26,4 +35,35 @@ function togglePlayerTurn() {
     active: tempInactive,
     inactive: tempActive
   };
+}
+
+function checkCols(player) {
+  return (
+    rows[0].children[0].classList.contains(player) && rows[1].children[0].classList.contains(player) && rows[2].children[0].classList.contains(player) ||
+    rows[0].children[1].classList.contains(player) && rows[1].children[1].classList.contains(player) && rows[2].children[1].classList.contains(player) ||
+    rows[0].children[2].classList.contains(player) && rows[1].children[2].classList.contains(player) && rows[2].children[2].classList.contains(player)
+  );
+}
+
+function checkRows(player) {
+  return (
+    rows[0].children[0].classList.contains(player) && rows[0].children[1].classList.contains(player) && rows[0].children[2].classList.contains(player) ||
+    rows[1].children[0].classList.contains(player) && rows[1].children[1].classList.contains(player) && rows[1].children[2].classList.contains(player) ||
+    rows[2].children[0].classList.contains(player) && rows[2].children[1].classList.contains(player) && rows[2].children[2].classList.contains(player)
+  );
+}
+
+function checkDiagonals(player) {
+  return (
+    rows[0].children[0].classList.contains(player) && rows[1].children[1].classList.contains(player) && rows[2].children[2].classList.contains(player) ||
+    rows[2].children[0].classList.contains(player) && rows[1].children[1].classList.contains(player) && rows[2].children[0].classList.contains(player)
+  );
+}
+
+function checkForWinner(player) {
+  if (checkCols(player) || checkRows(player) || checkDiagonals(player)) {
+    winner = player;
+    winnerEl.textContent = winner + ' Wins!';
+    running = false;
+  }
 }
